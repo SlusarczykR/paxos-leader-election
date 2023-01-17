3c43b3f1683f82fd3bb340eb66be39b01f271fc8
SlusarczykRad <radoslaw.slusarczyk@leovegas.com>
2023-01-17 20:03:41 +0100

    add swagger support

--- src/main/java/com/slusarczykr/paxos/leader/PaxosLeaderElectionApplication.java
+++ src/main/java/com/slusarczykr/paxos/leader/PaxosLeaderElectionApplication.java
@@ -4,8 +4,10 @@ import com.slusarczykr.paxos.leader.election.config.LeaderElectionProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 @EnableConfigurationProperties(LeaderElectionProperties.class)
 public class PaxosLeaderElectionApplication {
 
